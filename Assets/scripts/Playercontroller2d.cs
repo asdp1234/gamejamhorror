5e37b6c503343649bfa9a9affcdf210e93a3463b
connor <connorswannell@me.com>
2020-08-15 18:19:35 +0100

    player con on its way

---
+++ Assets/scripts/Playercontroller2d.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller2d : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5;
+    [SerializeField]
+    Rigidbody2D rb;
+    [SerializeField]
+    Vector2 move = new Vector2(0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        move.x = Input.GetAxis("Horizontal");
+        move.y = Input.GetAxis("Vertical");
+
+        rb.AddForce(move * speed);
+    }
+
+    void Update()
+    {
+        
+    }
+}
